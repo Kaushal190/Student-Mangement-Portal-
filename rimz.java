2353e8322941e0276b470b0f10f138e8b88d8b70
Kaushal190 <kaushalmotwani63@gmail.com>
2024-10-18 12:56:43 +0530

    Add files via upload

---
+++ rimz.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class rimz {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your age===");
+        int age = sc.nextInt();
+        if(age >= 18){
+            System.out.println("Welcome for Voting");
+        }else{
+            System.out.println("Invalid age");
+        }
+    }
+
+}
